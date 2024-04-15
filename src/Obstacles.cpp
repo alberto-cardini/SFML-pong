4beea41abb160d04752e62a5acb079bac7753ec6
alberto-cardini <albycardini@gmail.com>
2024-04-15 22:17:28 +0200

    Objects' random fill color

--- src/Obstacles.cpp
+++ src/Obstacles.cpp
@@ -5,10 +5,14 @@
 
 Obstacle::Obstacle() {
     int vertex = 3 + (rand() % 5);
-    int offset = 500;
 
     entity = sf::CircleShape(15 + rand() % 30, vertex);
+    entity.setFillColor(sf::Color(
+            10 + rand() % 244,
+            10 + rand() % 244,
+            10 + rand() % 244));
     setPosition(150 + rand() % 350, 100 + rand() % 350);
 
 }
 
+//static_cast<sf::Color>(rand() % 8)
