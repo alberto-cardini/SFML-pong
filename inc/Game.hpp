6564a99ae52f4d782780c1e16bf48cf0719668d1
alberto-cardini <albycardini@gmail.com>
2024-04-16 22:43:57 +0200

    Objects respawn responsibility given to class Game. Major refactor of the logic.

--- inc/Game.hpp
+++ inc/Game.hpp
@@ -4,49 +4,49 @@
 #ifndef GAME_hpp
 #define GAME_hpp
 
-#include "SFML/Graphics.hpp"
-#include "User.hpp"
 #include "Ball.hpp"
 #include "Obstacles.hpp"
+#include "SFML/Graphics.hpp"
+#include "User.hpp"
 
-class Game{
+class Game {
 public:
-
     Game();
 
     void render();
     void renderHUD();
     void update();
-    void eventManager();
+    void manageEvent();
     void run();
     void restart();
 
-    sf::Vector2f getVector(const Obstacle& obs) const;
     void hit();
+    void spawnObs();
 
 private:
-
     User player;
     Ball ball;
     std::vector<Obstacle> obs;
 
-    static float maxHeight;  //counting 0 form the top
+    static float maxHeight;  // counting 0 form the top
     static float minHeight;
-    static int score;
-
-    //Window
+    static sf::Time spawnTime;
+    // Window
     sf::RenderWindow window;
-    //HUD
-    sf::Text scoreText;
+    // HUD
+    sf::Text HUD;
     sf::Font font;
-    //Border
+    // Border
     sf::RectangleShape top;
     sf::RectangleShape bot;
     sf::RectangleShape right;
     sf::RectangleShape left;
-    //Time capture
-    sf::Clock clock;
+    // Time capture
+    sf::Clock dtClock;
+    sf::Clock gameClock;
+    sf::Clock spawnClock;
     sf::Time deltaTime;
+    sf::Time gameTime;
 };
 
 #endif
--- src/Game.cpp
+++ src/Game.cpp
@@ -4,89 +4,107 @@
 #include "Game.hpp"
 #include <iostream>
 
-bool spawn = false;
-int Game::score = 0;
-int i = 0;
-float Game::maxHeight = 150;  //counting 0 form the top
+sf::Time Game::spawnTime = sf::seconds(5);
+float Game::maxHeight = 150;  // counting 0 form the top
 float Game::minHeight = 450;
 
 Game::Game() : window(sf::VideoMode(800, 600), "Pong", sf::Style::Close) {
-
     window.setFramerateLimit(60);
 
-    //HUD
-    if(!font.loadFromFile("font/OpenSans-Regular.ttf")){
+    // HUD
+    if (!font.loadFromFile("font/OpenSans-Regular.ttf")) {
         perror("Wrong working directory");
     }
-    scoreText.setFont(font);
-    scoreText.setPosition(100,50);
+    HUD.setFont(font);
+    HUD.setPosition(100, 50);
 
-    //Border
+    // Border
     top = sf::RectangleShape(sf::Vector2f(500, 6));
     bot = sf::RectangleShape(sf::Vector2f(500, 6));
     right = sf::RectangleShape(sf::Vector2f(6, 412));
     left = sf::RectangleShape(sf::Vector2f(6, 412));
 
-    top.setOrigin(250,3);
-    bot.setOrigin(250,3);
+    left.setFillColor(sf::Color::Red);
 
-    right.setOrigin(3,200);
-    left.setOrigin(3,200);
+    top.setOrigin(250, 3);
+    bot.setOrigin(250, 3);
 
-    top.setPosition(400,97);
-    bot.setPosition(400,503);
+    right.setOrigin(3, 200);
+    left.setOrigin(3, 200);
 
-    left.setPosition(147,294);
-    right.setPosition(653,294);
+    top.setPosition(400, 97);
+    bot.setPosition(400, 503);
 
-    //Entities
+    left.setPosition(147, 294);
+    right.setPosition(653, 294);
+
+    // Entities
     player = User();
     ball = Ball();
 
-    //Time
-    clock = sf::Clock();
+    // Time
+    dtClock = sf::Clock();
+    gameClock = sf::Clock();
+    spawnClock = sf::Clock();
+    gameTime = sf::seconds(20);
 }
 
 void Game::renderHUD() {
-    sf::String string = "SCORE: " + std::to_string(score);
-    scoreText.setString(string);
+    sf::String string =
+        "TIME LEFT: " +
+        std::to_string(static_cast<int>(
+            gameTime.asSeconds() - gameClock.getElapsedTime().asSeconds())) +
+        " sec.";
+    HUD.setString(string);
     window.draw(top);
     window.draw(bot);
     window.draw(right);
     window.draw(left);
 }
 
-void Game::render(){
+void Game::render() {
     window.clear();
     renderHUD();
-    window.draw(scoreText);
+    window.draw(HUD);
     window.draw(ball);
     window.draw(player);
-    for(const Obstacle& n : obs)
+    for (const Obstacle& n : obs) {
         window.draw(n);
+        // window.draw(n.timeGain);
+    }
     window.display();
 }
 
 void Game::update() {
-    deltaTime = clock.restart();
-    ball.move(deltaTime);
-    hit();
-    static long seed;
-    seed = std::time(nullptr) + seed;
-    srand(seed);
+    if (gameClock.getElapsedTime().asSeconds() < gameTime.asSeconds()) {
+        deltaTime = dtClock.restart();
+        ball.move(deltaTime);
+        hit();
+        static long seed;
+        seed = std::time(nullptr) + seed;
+        srand(seed);
+        if (static_cast<int>(spawnClock.getElapsedTime().asSeconds() -
+                             spawnTime.asSeconds()) == 0) {
+            spawnClock.restart();
+            spawnObs();
+        }
+    } else {
+        gameClock.restart();
+        restart();
+    }
 }
 
-void Game::restart(){
-    score = 0;
+void Game::restart() {
+    gameTime = sf::seconds(20);
+    gameClock.restart();
     ball.setPosition(400, 300);
     obs.clear();
 }
 
-void Game::eventManager(){
+void Game::manageEvent() {
     sf::Event event;
-    while(window.pollEvent(event)){
+    while (window.pollEvent(event)) {
         switch (event.type) {
-
             case sf::Event::Closed:
                 window.close();
                 break;
@@ -94,14 +112,13 @@ void Game::eventManager(){
             case sf::Event::KeyPressed:
 
                 switch (event.key.scancode) {
-
-                    case sf::Keyboard::Scan::Down:
-                        if(player.getPosition().y + 20 < minHeight)
-                            player.move(0,20);
+                    case sf::Keyboard::Scan::S:
+                        if (player.getPosition().y + 20 < minHeight)
+                            player.move(0, 20);
                         break;
 
-                    case sf::Keyboard::Scan::Up:
-                        if(player.getPosition().y - 20 > maxHeight)
+                    case sf::Keyboard::Scan::W:
+                        if (player.getPosition().y - 20 > maxHeight)
                             player.move(0, -20);
                         break;
 
@@ -116,55 +133,54 @@ void Game::eventManager(){
     }
 }
 
-void Game::run(){
-    while(window.isOpen()){
-        eventManager();
+void Game::run() {
+    while (window.isOpen()) {
+        manageEvent();
         update();
         render();
     }
 }
 
-void Game::hit(){ //remember that the y-axis is flipped, down is positive and up negative. x-axis is OK
+void Game::spawnObs() {
+    Obstacle newObs;
+    obs.push_back(newObs);
+}
+
+void Game::hit() {  // remember that the y-axis is flipped, down is positive and
+                    // up negative. x-axis is OK
 
     for (int i = 0; i < obs.size(); i++) {
         if (ball.getGlobalBounds().intersects(obs[i].getGlobalBounds())) {
-            ball.setVelocity(ball.getVelocity().x * (-1), ball.getVelocity().y * (1));
-            obs.erase( obs.end() - (obs.size() - i));
-            score++;
+            ball.setVelocity(ball.getVelocity().x * (-1),
+                             ball.getVelocity().y * (1));
+            obs.erase(obs.end() - (obs.size() - i));
+            gameTime += obs[i].getTimeGain();
         }
     }
 
-    if (ball.getGlobalBounds().intersects(player.getGlobalBounds())){
-        score++;
-        ball.setVelocity(ball.getVelocity().x * (-1), ball.getVelocity().y * (1));
-        Obstacle newObs;
-        obs.push_back(newObs);
-        std::cout << ball.getVelocity().x << " " << ball.getVelocity().y << std::endl;
+    if (ball.getGlobalBounds().intersects(player.getGlobalBounds())) {
+        ball.setVelocity(ball.getVelocity().x * (-1),
+                         ball.getVelocity().y * (1));
     }
 
-    if (ball.getGlobalBounds().intersects(top.getGlobalBounds())){
-        ball.setVelocity(ball.getVelocity().x * (1), ball.getVelocity().y * (-1));
-        std::cout << ball.getVelocity().x << " " << ball.getVelocity().y << std::endl;
+    if (ball.getGlobalBounds().intersects(top.getGlobalBounds())) {
+        ball.setVelocity(ball.getVelocity().x * (1),
+                         ball.getVelocity().y * (-1));
     }
 
-    if (ball.getGlobalBounds().intersects(bot.getGlobalBounds())){
-        ball.setVelocity(ball.getVelocity().x * (1), ball.getVelocity().y * (-1));
-        std::cout << ball.getVelocity().x << " " << ball.getVelocity().y << std::endl;
+    if (ball.getGlobalBounds().intersects(bot.getGlobalBounds())) {
+        ball.setVelocity(ball.getVelocity().x * (1),
+                         ball.getVelocity().y * (-1));
     }
 
-    if (ball.getGlobalBounds().intersects(left.getGlobalBounds())){
-        ball.setVelocity(ball.getVelocity().x * (-1), ball.getVelocity().y * (1));
-        std::cout << ball.getVelocity().x << " " << ball.getVelocity().y << std::endl;
-        //restart();
+    if (ball.getGlobalBounds().intersects(left.getGlobalBounds())) {
+        ball.setVelocity(ball.getVelocity().x * (-1),
+                         ball.getVelocity().y * (1));
+        gameTime -= sf::seconds(3);
     }
 
-    if (ball.getGlobalBounds().intersects(right.getGlobalBounds())){
-        ball.setVelocity(ball.getVelocity().x * (-1), ball.getVelocity().y * (1));
-        std::cout << ball.getVelocity().x << " " << ball.getVelocity().y << std::endl;
+    if (ball.getGlobalBounds().intersects(right.getGlobalBounds())) {
+        ball.setVelocity(ball.getVelocity().x * (-1),
+                         ball.getVelocity().y * (1));
     }
 }
-
-/*
-sf::Vector2f Game::getVector(const Obstacle& obs) const {
-
-}*/
