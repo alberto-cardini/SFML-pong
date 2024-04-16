980dba72a9beb5e27ecf5b0f6dfee71d89a330f5
alberto-cardini <albycardini@gmail.com>
2024-04-16 22:41:03 +0200

    add Obstacles' timeGain based on vertex number.

--- src/Obstacles.cpp
+++ src/Obstacles.cpp
@@ -3,14 +3,24 @@
 //
 #include "Obstacles.hpp"
 
+#include <iostream>
+
 Obstacle::Obstacle() {
-    int vertex = 3 + (rand() % 5);
+    int x = 150 + rand() % 350;
+    int y = 100 + rand() % 350;
 
-    entity = sf::CircleShape(15 + rand() % 30, vertex);
-    entity.setFillColor(sf::Color(
-            10 + rand() % 244,
-            10 + rand() % 244,
-            10 + rand() % 244));
-    setPosition(150 + rand() % 350, 100 + rand() % 350);
+    vertex = 3 + (rand() % 4);
+    timeGain = sf::seconds(vertex);
+    entity = sf::CircleShape(pow(vertex - 10, 2), vertex);
+    entity.setFillColor(
+        sf::Color(10 + rand() % 244, 10 + rand() % 244, 10 + rand() % 244));
+    setPosition(x, y);
 
+    /*
+    if (!font.loadFromFile("font/OpenSans-Regular.ttf"))
+        perror("Wrong working directory");
+
+        sf::String string = std::to_string(vertex) + " sec";
+        textTimeGain = sf::Text(string, font, 15);
+        textTimeGain.setPosition(x, y);*/
 }
